e170bff81fcaa2e66347e43a1f70b780b31625c3
NavikkhaArvind <navikkhaarvind6@gmail.com>
2026-07-18 18:23:58 +0530

    Update 06_Business_Insights.sql

--- SQL/06_Business_Insights.sql
+++ SQL/06_Business_Insights.sql
@@ -1,25 +1,45 @@
--- View 1
-CREATE VIEW hotel_revenue AS
+Query 1: Which hotels generate the highest revenue?
 SELECT
     hotel_name,
     SUM(amount_paid) AS total_revenue
 FROM hotel_bookings
-GROUP BY hotel_name;
+GROUP BY hotel_name
+ORDER BY total_revenue DESC;
 
---View 2
 
-CREATE VIEW city_revenue AS
+
+Query 2: Which cities generate the highest revenue?
 SELECT
     city,
     SUM(amount_paid) AS total_revenue
 FROM hotel_bookings
-GROUP BY city;
+GROUP BY city
+ORDER BY total_revenue DESC;
 
---View 3
-CREATE VIEW room_type_summary AS
+
+
+Query 3: Which room type is booked the most?
 SELECT
     room_type,
-    COUNT(*) AS total_bookings,
-    AVG(amount_paid) AS average_payment
+    COUNT(*) AS total_bookings
 FROM hotel_bookings
-GROUP BY room_type;
+GROUP BY room_type
+ORDER BY total_bookings DESC;
+
+
+
+Query 4: Which payment method is used the most?
+SELECT
+    payment_method,
+    COUNT(*) AS total_transactions
+FROM hotel_bookings
+GROUP BY payment_method
+ORDER BY total_transactions DESC;
+
+
+
+Query 5: How many bookings were cancelled?
+SELECT
+    COUNT(*) AS cancelled_bookings
+FROM hotel_bookings
+WHERE booking_status = 'Cancelled';
