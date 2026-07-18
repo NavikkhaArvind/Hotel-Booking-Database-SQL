84a6b6aa04a42eeb8c997e510b835332a1bf6a56
NavikkhaArvind <navikkhaarvind6@gmail.com>
2026-07-18 17:48:26 +0530

    Update 03_Insert_Data.sql

--- SQL/03_Insert_Data.sql
+++ SQL/03_Insert_Data.sql
@@ -1,1 +1,14 @@
+-- ==========================================
+-- Hotel Booking Database Project
+-- File: 03_Insert_Data.sql
+-- ==========================================
 
+-- Data was imported into the hotel_bookings table
+-- using MySQL Workbench's Table Data Import Wizard.
+
+-- Dataset:
+-- Hotel_Booking_Dataset.csv
+
+-- Total Records Imported:
+SELECT COUNT(*) AS total_records
+FROM hotel_bookings;
