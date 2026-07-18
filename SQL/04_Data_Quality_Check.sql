82ca65553064be262fa232fba8a4ca4d0d2bd800
NavikkhaArvind <navikkhaarvind6@gmail.com>
2026-07-18 17:54:11 +0530

    Update 04_Data_Quality_Check.sql

--- SQL/04_Data_Quality_Check.sql
+++ SQL/04_Data_Quality_Check.sql
@@ -1,1 +1,17 @@
+SELECT COUNT(*) FROM hotel_bookings;
 
+SELECT
+SUM(booking_id IS NULL),
+...
+FROM hotel_bookings;
+
+SELECT booking_id, COUNT(*)
+FROM hotel_bookings
+GROUP BY booking_id
+HAVING COUNT(*) > 1;
+
+SELECT * FROM hotel_bookings LIMIT 10;
+
+SELECT booking_status, COUNT(*)
+FROM hotel_bookings
+GROUP BY booking_status;
