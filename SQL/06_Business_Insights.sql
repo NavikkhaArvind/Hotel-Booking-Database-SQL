27f1e335bdb0917bd7ef4b801d1d810cb8a0624a
NavikkhaArvind <navikkhaarvind6@gmail.com>
2026-07-18 18:19:18 +0530

    Update 06_Business_Insights.sql

--- SQL/06_Business_Insights.sql
+++ SQL/06_Business_Insights.sql
@@ -1,1 +1,25 @@
+-- View 1
+CREATE VIEW hotel_revenue AS
+SELECT
+    hotel_name,
+    SUM(amount_paid) AS total_revenue
+FROM hotel_bookings
+GROUP BY hotel_name;
 
+--View 2
+
+CREATE VIEW city_revenue AS
+SELECT
+    city,
+    SUM(amount_paid) AS total_revenue
+FROM hotel_bookings
+GROUP BY city;
+
+--View 3
+CREATE VIEW room_type_summary AS
+SELECT
+    room_type,
+    COUNT(*) AS total_bookings,
+    AVG(amount_paid) AS average_payment
+FROM hotel_bookings
+GROUP BY room_type;
